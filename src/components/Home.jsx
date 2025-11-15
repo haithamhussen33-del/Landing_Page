e21df7ec94b980577ae9b457447714f086685473
haithamhussen33-del <haitham.hussen33@gmail.com>
2025-11-15 05:45:15 +0300

    fixed some staff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,21 @@ import "../styles/HomePageStyles.css";
 import CardRoulet from "./CardRoulet";
 import ServicesFlip from "./ServicesFlip";
 import HeroCarousel from "./HeroCarousel";
+import CardTest from "./CardTest";
 export default function Home() {
   return (
     <div className="home">
       <HeroCarousel />
-      <br></br>
-      <br></br>
 
       <ServicesFlip />
       <br />
       <br />
       <CardRoulet />
-
+      <br />
+      <br />
+      <CardTest />
+      <br />
+      <br />
       <section className="about">
         <div className="about-content">
           <h2>About Our Company</h2>
